51c8403b83be4397e0ce7f281c2415e84bffd50f
Simon Petit <simon@MacBook-Pro-de-Simon.local>
2017-06-29 23:14:52 +0200

    initial commit

---
+++ template/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <h1>Simon petit</h1>
+    <p>Checkout my <a href="https://www.github.com/SiwonP">github repo</a></p>
+
+    <div>
+      <p>Mes projets</p>
+      <ul>
+        <li>Correcteur orthographique en C</li>
+        <li>Compilateur en C</li>
+        <li>Videos blender</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ template/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path')
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/src')
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/src")
+  }
+
+}
