d8a365a340734d3181a48b7382c99325ef1f87d5
Simon Petit <simon@MacBook-Pro-de-Simon.local>
2017-06-29 23:26:32 +0200

    modifying app.vue to make it sober

--- template/components/App.vue
+++ template/components/App.vue
@@ -1,18 +1,7 @@
 <template>
-  <div>
-
-    <h1>Simon petit</h1>
-    <p>Checkout my <a href="https://www.github.com/SiwonP">github repo</a></p>
-
-    <div>
-      <p>Mes projets</p>
-      <ul>
-        <li>Correcteur orthographique en C</li>
-        <li>Compilateur en C</li>
-        <li>Videos blender</li>
-      </ul>
-    </div>
-  </div>
+<div>
+  <h1>Welcome to your new Vue app</h1>
+</div>
 </template>
 
 <script>
