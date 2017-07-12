1487a12acf53f9afdc4e9caa7872674b9b2c5061
Simon Petit <nomisp96@hotmail.fr>
2017-07-12 22:34:32 +0200

    adding css-loader

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -2,24 +2,27 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    publicPath: "/dist/",
-    path: path.join(__dirname, '/dist')
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        publicPath: "/dist/",
+        path: path.join(__dirname, '/dist')
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }]
+    },
+    devServer: {
     }
-  },
-  module: {
-    rules: [{
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    }]
-  },
-  devServer: {
-  }
 
 }
