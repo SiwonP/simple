79e9a419f6e3b38d1bc9b31e6ddf820ac1454dd6
Simon Petit <nomisp96@hotmail.fr>
2017-12-23 04:34:12 +0100

    prod environnement to get rid of warnings

--- template/webpack.prod.js
+++ template/webpack.prod.js
@@ -3,7 +3,12 @@ const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
-  ]
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true}),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ]
 })
